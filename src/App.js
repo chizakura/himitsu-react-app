de9190bf439839708d1c7b149644eed04934a518
Linda Chin <lindapchin225@gmail.com>
2019-05-25 14:45:43 -0400

    added working search bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import './App.css';
 import SearchBar from './Components/SearchBar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchValue: ""
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleInputChange(value) {
+    this.setState({
+      searchValue: value
+    })
+  }
+
+  handleSearch() {
+  }
+
   render() {
     return (
       <div className="app-header">
@@ -10,7 +30,10 @@ class App extends Component {
           <img className="logo" src="/favicon.ico" alt="logo"/>
           <h1>HIMITSU</h1>
         </div>
-        <SearchBar/>
+        <SearchBar
+          handleInputChange={this.handleInputChange}
+          searchValue={this.state.searchValue}
+        />
       </div>
     )
   }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-    return (
-        <form>
-            <label name="search">Search: </label>
-            <input type="text" name="search"></input>
-        </form>
-    )
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const value = event.target.value;
+        this.props.handleInputChange(value);
+        
+    }
+
+    render() {
+        return (
+            <form>
+                <label name="search">Search: </label>
+                <input name="search" type="text" value={this.props.searchValue} onChange={this.handleChange}/>
+            </form>
+        )
+    }
 }
 
 export default SearchBar;
